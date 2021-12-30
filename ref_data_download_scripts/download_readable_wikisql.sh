f79fa02a065ad42d84990c0d391a4d774d1decc6
Ilmar Uduste <ilmaruduste@gmail.com>
2021-12-30 21:28:47 +0200

    Fix data path in wikisql

--- ref_data_download_scripts/download_readable_wikisql.sh
+++ ref_data_download_scripts/download_readable_wikisql.sh
@@ -1,4 +1,5 @@
 mkdir wikisql
 python -c "import gdown; gdown.download('https://drive.google.com/uc?export=download&id=1TBT9MnaGOUcQ3rCXo2KAXXUYEwkrY133', 'wikisql/wikisql.zip', quiet=False)"
-unzip wikisql/wikisql.zip
-rm wikisql/wikisql.zip
+cd wikisql
+unzip wikisql.zip
+rm wikisql.zip
